aa3fe69311ad368f4dadfaa0ff69bfa60e4d9491
root <root@s34-v06.(none)>
2015-08-13 15:04:23 +0800

    delete outdate

--- z_bak/desktop_back.sh
+++
@@ -1,8 +0,0 @@
-dir_from="/Users/mengwang/Work/root_git/util"
-dir_to="/Users/mengwang/百度云同步盘/Work/util"
-dir_log="/Users/mengwang/Work/bin"
-
-mkdir -p $dir_to
-rm -rf $dir_to/*
-cp -r $dir_from/[0-9a-z]_* $dir_to/
-date
